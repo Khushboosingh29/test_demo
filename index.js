41a805933d99cf897a98cee4f7a8dc2d10ae0b36
Khushboosingh29 <93135311+Khushboosingh29@users.noreply.github.com>
2022-06-15 23:23:29 +0530

    Add files via upload
    final one

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-
 const express = require("express");
 //var http = require("http");
 const app = express();
-const port= process.env.Port || 5000;
+
+const port= process.env.PORT || 5000;
 
 const mongoose=require("mongoose");
 const router = require("./routes/issues");
-mongoose.connect('mongodb+srv://khushboohavoc:khushboo123@cluster0.yxd9d.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
-//mongoose.connect('mongodb+srv://Issue:9Anushka219@cluster0.yodvb.mongodb.net/?retryWrites=true&w=majority');
+//mongoose.connect('mongodb+srv://khushboohavoc:khushboo123@cluster0.yxd9d.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://Issue:9Anushka219@cluster0.yodvb.mongodb.net/?retryWrites=true&w=majority');
 const db=mongoose.connection;
 db.on("error",console.error.bind(console,"connection error: "));
 db.once("open",function(){
     console.log("Connected MongoDB");
 });
 
-app.listen(process.env.PORT,function(){
+app.listen(port,function(){
     console.log("Server is running on port", port);
 });
 
